cade33bb276a7a8ce83027a78cbfac0b10e7c863
Anatol Belski <anbelski@linux.microsoft.com>
2022-11-13 19:26:36 +0000

    hv: reboot: Implement EnterSleepState hypecrall
    The hypercall is issued in the callabck attached to the reboot syscall
    notification. The reboot notifier appears to be equivalent to those
    provided for ACPI, however it appears to be more reliable as it'll be
    triggered also when ACPI support is not present.
    
    Internally, the EnterSleepState hypercall needs to have a sleep state
    prepared in the hypervisor. The sleep state initialization is done
    through another call HvSetSystemProperty. This patch adds the
    rudimentary parts necessary for this hypercall to succeed setting sleep
    states. More system properties are available to set and thus the scructs
    and use cases can be extended as the development is going forward.
    
    Fixes: #40833429
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>
    
    hv: Add ACPI sleep notify callbacks
    
    This goes in addition to already existing EnterSleepState integration
    using the shutdown notifier mechanism.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>
    
    drivers: hv: Set proper PM values during sleep state init
    
    While initializing sleep states in hv, get correct PM values from ACPI and
    pass them to hv.
    
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>
    
    drivers: hv: reorder hv_initialize_sleep_states
    
    hv_initialize_sleep_states should be invoked after ACPI is initialized.
    Invoke this method in shutdown sequence, just before in transitioning hv
    to a sleep state.
    
    As hv_initialize_sleep_states is now invoked in shutdown sequence, ACPI
    will be in an initialized state and acpi_get_sleep_type_data will return
    proper PM values to be passed to hv.
    
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>
    Signed-off-by: Stanislav Kinsburskii <skinsburskii@linux.microsoft.com>

--- arch/x86/hyperv/hv_init.c
+++ arch/x86/hyperv/hv_init.c
@@ -545,6 +545,13 @@ void __init hyperv_init(void)
 
 		/* mark ram reserved for hypervisor as owned by hypervisor */
 		hv_mark_resources();
+
+		/*
+		 * The notifier registration might fail at multiple hops. Corresponding
+		 * error messages will land in dmesg. There is otherwise nothing
+		 * that can be specifically done to handle this failure.
+		 */
+		(void)hv_sleep_notifiers_register();
 	} else {
 		hypercall_msr.guest_physical_address = vmalloc_to_pfn(hv_hypercall_pg);
 		wrmsrl(HV_X64_MSR_HYPERCALL, hypercall_msr.as_uint64);
