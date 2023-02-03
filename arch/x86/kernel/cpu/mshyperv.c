c4b433fcffde080f641f9f44fa69cf369b0371a6
Stanislav Kinsburskiy <stanislav.kinsburskiy@gmail.com>
2023-02-03 18:49:57 +0000

    x86/hyperv: Pass on the lpj value from host to guest
    And have it preset.
    This change allows to significantly reduce time to bring up guest SMP
    configuration as well as make sure the guest won't get inaccurate calibration
    results due to "noisy neighbour" situation.
    
    Below are the numbers for 16 VCPU guest before the patch (~1300 msec)
    
    [    0.562938] x86: Booting SMP configuration:
    ...
    [    1.859447] smp: Brought up 1 node, 16 CPUs
    
    and after the patch (~130 msec):
    
    [    0.445079] x86: Booting SMP configuration:
    ...
    [    0.575035] smp: Brought up 1 node, 16 CPUs
    
    This change is inspired by commit 0293615f3f.
    
    Signed-off-by: Stanislav Kinsburskiy <stanislav.kinsburskiy@gmail.com>
    Signed-off-by: Anirudh Rayabharam <anrayabh@linux.microsoft.com>

--- arch/x86/kernel/cpu/mshyperv.c
+++ arch/x86/kernel/cpu/mshyperv.c
@@ -439,6 +439,21 @@ int hv_get_hypervisor_version(union hv_hypervisor_version_info *info)
 }
 EXPORT_SYMBOL_GPL(hv_get_hypervisor_version);
 
+static void __init __maybe_unused hv_preset_lpj(void)
+{
+	unsigned long khz;
+	u64 lpj;
+
+	if (!x86_platform.calibrate_tsc)
+		return;
+
+	khz = x86_platform.calibrate_tsc();
+
+	lpj = ((u64)khz * 1000);
+	do_div(lpj, HZ);
+	preset_lpj = lpj;
+}
+
 static void __init ms_hyperv_init_platform(void)
 {
 	int hv_max_functions_eax;
@@ -645,6 +660,12 @@ static void __init ms_hyperv_init_platform(void)
 	/* Register Hyper-V specific clocksource */
 	hv_init_clocksource();
 	hv_vtl_init_platform();
+
+	/*
+	 * Preset lpj to make calibrate_delay a no-op, which is turn helps to
+	 * speed up secondary cores initialization.
+	 */
+	hv_preset_lpj();
 #endif
 	/*
 	 * TSC should be marked as unstable only after Hyper-V
