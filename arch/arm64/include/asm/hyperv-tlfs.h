7b68e43cbce37e193cc1fb44299d5b8dfbfae08d
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2023-03-07 19:07:33 +0000

    hyperv-tlfs: Change shared HV_REGISTER_* defines to HV_SYNTHETIC_REG_*
    In x86 hyperv-tlfs, HV_REGISTER_ prefix is used to indicate MSRs
    accessed via rdmsrl/wrmsrl. But in ARM64, HV_REGISTER_ instead indicates
    VP registers accessed via get/set vp registers hypercall.
    
    This is due to HV_REGISTER_* names being used by hv_set/get_register,
    with the arch-specific version delegating to the appropriate mechanism.
    
    The problem is, using prefix HV_REGISTER_ for MSRs will conflict with
    VP registers when they are introduced for x86 in future.
    
    This patch solves the issue by:
    
    1. Defining all the x86 MSRs with a consistent prefix: HV_X64_MSR_.
       This is so HV_REGISTER_ can be reserved for VP registers.
    
    2. Change the non-arch-specific alias used by hv_set/get_register to
       HV_SYN_REG.
    
    Also fix incorrect namings in arm64 hyperv-tlfs.h
    
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

--- arch/arm64/include/asm/hyperv-tlfs.h
+++ arch/arm64/include/asm/hyperv-tlfs.h
@@ -14,6 +14,7 @@
 #define _ASM_HYPERV_TLFS_H
 
 #include <linux/types.h>
+#include <asm-generic/hyperv-common-types.h>
 
 /*
  * All data structures defined in the TLFS that are shared between Hyper-V
@@ -40,19 +41,35 @@
 #define HV_STIMER_DIRECT_MODE_AVAILABLE		BIT(13)
 
 /*
- * Synthetic register definitions equivalent to MSRs on x86/x64
+ * Synthetic register definitions name differently in hyperv-common-types.h
  */
-#define HV_REGISTER_CRASH_P0		0x00000210
-#define HV_REGISTER_CRASH_P1		0x00000211
-#define HV_REGISTER_CRASH_P2		0x00000212
-#define HV_REGISTER_CRASH_P3		0x00000213
-#define HV_REGISTER_CRASH_P4		0x00000214
-#define HV_REGISTER_CRASH_CTL		0x00000215
+#define HV_REGISTER_SIMP		HV_REGISTER_SIPP
+#define HV_REGISTER_SIEFP		HV_REGISTER_SIFP
 
-#define HV_REGISTER_GUEST_OSID		0x00090002
+/*
+ * To support non-arch-specific code calling hv_set/get_register:
+ * - On x86,   HV_SYN_REG_ indicates an MSR accessed via rdmsrl/wrmsrl
+ * - On ARM64, HV_SYN_REG_ indicates a VP register accessed via hypercall
+ */
+#define HV_SYN_REG_VP_INDEX		(HV_REGISTER_VP_INDEX)
+#define HV_SYN_REG_TIME_REF_COUNT	(HV_REGISTER_TIME_REF_COUNT)
+#define HV_SYN_REG_REFERENCE_TSC	(HV_REGISTER_REFERENCE_TSC)
+#define HV_SYN_REG_STIMER0_CONFIG	(HV_REGISTER_STIMER0_CONFIG)
+#define HV_SYN_REG_STIMER0_COUNT	(HV_REGISTER_STIMER0_COUNT)
 
-#define HV_REGISTER_SIEFP		0x000A0012
-#define HV_REGISTER_SIMP		0x000A0013
+#define HV_SYN_REG_SCONTROL		(HV_REGISTER_SCONTROL)
+#define HV_SYN_REG_SIEFP		(HV_REGISTER_SIFP)
+#define HV_SYN_REG_SIMP			(HV_REGISTER_SIMP)
+#define HV_SYN_REG_SIRBP		(HV_REGISTER_SIRBP)
+#define HV_SYN_REG_EOM			(HV_REGISTER_EOM)
+#define HV_SYN_REG_SINT0		(HV_REGISTER_SINT0)
+
+#define HV_SYN_REG_CRASH_P0		(HV_REGISTER_GUEST_CRASH_P0)
+#define HV_SYN_REG_CRASH_P1		(HV_REGISTER_GUEST_CRASH_P1)
+#define HV_SYN_REG_CRASH_P2		(HV_REGISTER_GUEST_CRASH_P2)
+#define HV_SYN_REG_CRASH_P3		(HV_REGISTER_GUEST_CRASH_P3)
+#define HV_SYN_REG_CRASH_P4		(HV_REGISTER_GUEST_CRASH_P4)
+#define HV_SYN_REG_CRASH_CTL		(HV_REGISTER_GUEST_CRASH_CTL)
 
 union hv_msi_entry {
 	u64 as_uint64[2];
