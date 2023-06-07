38813bd0a2fb190782384dcae4e2b93e1fd6a656
Jinank Jain <jinankjain@linux.microsoft.com>
2023-06-07 15:27:27 +0000

    uapi: hyperv: Add sev_control to hv_register_value
    Since we are going to use hv_register_value to set the value of sev
    control register.
    
    Signed-off-by: Jinank Jain <jinankjain@linux.microsoft.com>

--- include/uapi/hyperv/hvgdk_mini.h
+++ include/uapi/hyperv/hvgdk_mini.h
@@ -1309,6 +1309,7 @@ union hv_register_value {
 	union hv_x64_pending_exception_event pending_exception_event;
 	union hv_x64_pending_virtualization_fault_event
 		pending_virtualization_fault_event;
+	union hv_x64_register_sev_control sev_control;
 #elif defined(__ARM64__)
 	union hv_arm64_pending_interruption_register pending_interruption;
 	union hv_arm64_interrupt_state_register interrupt_state;
