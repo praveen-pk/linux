f988f43e7353d0bdaec61116537c82f48d6f1c9d
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2023-06-27 18:07:14 +0000

    fixup! hyperv: Add a helper function to set sev control register

--- drivers/hv/mshv.h
+++ drivers/hv/mshv.h
@@ -176,7 +176,6 @@ int hv_call_write_gpa(u32 vp_index,
 		u8 *data,
 		u32 bytes_count,
 		union hv_access_gpa_result *result);
-
 int hv_call_issue_psp_guest_request(
 	u64 partition_id, u64 req_pfn, u64 rsp_pfn,
 	void (*completion_handler)(void * /* data */, u64 * /* status */),
--- drivers/hv/mshv_root_main.c
+++ drivers/hv/mshv_root_main.c
@@ -1929,6 +1929,26 @@ static void mshv_destroy_devices(struct mshv_partition *partition)
 	}
 }
 
+static int
+set_sev_control_register(u32 vp_index, u64 partition_id,
+			 u64 enable_encrypted_state,
+			 u64 vmsa_gpa_page_number)
+{
+	union hv_input_vtl input_vtl;
+	struct hv_register_assoc sev_control = {
+		.name = HV_X64_REGISTER_SEV_CONTROL,
+	};
+	union hv_x64_register_sev_control *sc;
+
+	sc = &sev_control.value.sev_control;
+	sc->enable_encrypted_state = enable_encrypted_state;
+	sc->vmsa_gpa_page_number = vmsa_gpa_page_number;
+
+	input_vtl.as_uint8 = 0;
+	return hv_call_set_vp_registers(vp_index, partition_id, 1, input_vtl,
+			&sev_control);
+}
+
 static long
 mshv_partition_ioctl_sev_snp_ap_create(struct mshv_partition *partition,
 				       void __user *user_args)
@@ -1959,7 +1979,7 @@ mshv_partition_ioctl_sev_snp_ap_create(struct mshv_partition *partition,
 		goto out;
 	}
 
-	ret = hv_set_sev_control_register(vp->index, vp->partition->id, 1,
+	ret = set_sev_control_register(vp->index, vp->partition->id, 1,
 					  HVPFN_DOWN(req.vmsa_gpa));
 	if (ret) {
 		vp_err(vp, "Failed to set sev control register\n");
@@ -2474,7 +2494,7 @@ static int destroy_snp_partition_state(struct mshv_partition *partition)
 		 * Clear the sev control register i.e., disable encrypted page and
 		 * VMSA GFN.
 		 */
-		ret = hv_set_sev_control_register(vp->index, vp->partition->id, 0, 0);
+		ret = set_sev_control_register(vp->index, vp->partition->id, 0, 0);
 		if (ret) {
 			vp_err(vp, "Failed to clear sev control register\n");
 			goto out;
