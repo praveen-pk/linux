169cde29f7b91a8fa11fc3a94117c71099f4b843
Anatol Belski <anbelski@linux.microsoft.com>
2023-08-11 05:23:18 +0000

    scripts: Fix version update pattern for makefile
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- pipelines/lsgtools/virt/mshv/kernel.py
+++ pipelines/lsgtools/virt/mshv/kernel.py
@@ -101,7 +101,7 @@ def update_kernel_version(src_tree, ver):
     with open(mf, "r") as f:
         mfc = f.read()
     with open(mf, "w") as f:
-        mfc = re.sub(r"EXTRAVERSION =\..*\d+\n", "EXTRAVERSION =.{}\\n".format(ver.split(".")[3]), mfc)
+        mfc = re.sub(r"EXTRAVERSION =\s*\..*\d+\n", "EXTRAVERSION =.{}\\n".format(ver.split(".")[3]), mfc)
         f.write(mfc)
 
     arch_map = {
