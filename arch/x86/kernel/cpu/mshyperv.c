b6bc48454f945e398409d20d7502c0498e46b34c
Nuno Das Neves <nudasnev@microsoft.com>
2023-08-30 22:39:58 +0000

    arch/x86/entry/vdso: Set TSC page as decrypted when mapping into userspace
    The TSC page is already marked as decrypted in the kernel via
    __bss_decrypted, but mapping it into userspace does not preserve the
    C-bit. The result is a (fatal) unaccepted GPA intercept in the VMM, but
    only when the page is accessed in userspace.
    
    On mapping, specify that the page should be decrypted with
    pgprot_decrypted(). Note that PV clock already does this.
    
    Signed-off-by: Nuno Das Neves <nudasnev@microsoft.com>

--- arch/x86/kernel/cpu/mshyperv.c
+++ arch/x86/kernel/cpu/mshyperv.c
@@ -647,7 +647,7 @@ static void __init ms_hyperv_init_platform(void)
 # endif
 
 	/* Register Hyper-V specific clocksource */
-	// hv_init_clocksource();
+	hv_init_clocksource();
 #endif
 	/*
 	 * TSC should be marked as unstable only after Hyper-V
