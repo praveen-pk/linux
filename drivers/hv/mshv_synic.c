dac09211823f2f510101fde2c831f36cfe9167c2
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2023-10-04 23:56:40 +0000

    Drivers: hv: Fix bad pointer dereference when handling apic eoi message
    hv_get_interrupt_vector_from_payload() is called with msg->u.payload[0],
    which would cast the first byte/s of the payload to a pointer.
    
    Instead, use &msg->u.payload[0] so the payload can be case to the apic
    eoi message struct.
    
    Remove hv_get_interrupt_vector_from_payload() altogether as it just
    obfuscates the casting operation, and does not belong in hvhdk.h anyway.
    
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

--- drivers/hv/mshv_synic.c
+++ drivers/hv/mshv_synic.c
@@ -356,8 +356,10 @@ mshv_intercept_isr(struct hv_message *msg)
 		 * to handle the VMEXIT. So we need not pass this message
 		 * to vcpu thread.
 		 */
-		if (mshv_notify_acked_gsi(partition,
-				hv_get_interrupt_vector_from_payload(msg->u.payload[0]))) {
+		struct hv_x64_apic_eoi_message *eoi_msg =
+			(struct hv_x64_apic_eoi_message *)&msg->u.payload[0];
+
+		if (mshv_notify_acked_gsi(partition, eoi_msg->interrupt_vector)) {
 			handled = true;
 			goto unlock_out;
 		}
