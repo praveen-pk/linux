335f248a5e3d6b227a4d84490facc509611bbefa
Anirudh Rayabharam <anrayabh@linux.microsoft.com>
2024-04-17 16:30:24 +0530

    x86/hyperv: add param to enable root VP spread
    In a minroot configuration, root VPs are created only on a subset of
    LPs. By default nodes are filled up with root VPs in order. node 0 is
    filled followed by node 1 followed by node 2 and so on.
    
    Add a kernel parameter that can be used to spread root VPs evenly across
    specified nodes for cases where we would need local root VPs in multiple
    nodes.
    
    Signed-off-by: Anirudh Rayabharam <anrayabh@linux.microsoft.com>

--- arch/x86/kernel/cpu/mshyperv.c
+++ arch/x86/kernel/cpu/mshyperv.c
@@ -40,6 +40,9 @@ bool hv_nested;
 struct ms_hyperv_info ms_hyperv;
 bool mshv_loader_new;
 
+static bool hv_minroot_nodes_defined __initdata;
+static nodemask_t hv_minroot_nodes __initdata;
+
 #if IS_ENABLED(CONFIG_HYPERV)
 static void (*mshv_handler)(void);
 static void (*vmbus_handler)(void);
@@ -305,6 +308,40 @@ static unsigned long hv_get_tsc_khz(void)
 
 	return freq / 1000;
 }
+
+static int __init hv_parse_root_vp_nodes(char *arg)
+{
+	char *tok;
+	int node, ret;
+
+	if (strcmp(arg, "all") == 0) {
+		nodes_setall(hv_minroot_nodes);
+	} else {
+		while ((tok = strsep(&arg, ",")) != NULL) {
+			ret = kstrtoint(tok, 10, &node);
+			if (ret) {
+				pr_warn("Hyper-V: invalid format for hv_root_vp_numa_nodes: %s\n",
+						arg);
+				return 0;
+
+			}
+
+			if (!node_possible(node)) {
+				pr_warn("Hyper-V: ignoring invalid node %u specified in hv_root_vp_numa_nodes.\n",
+						node);
+				continue;
+			}
+
+			node_set(node, hv_minroot_nodes);
+		}
+	}
+
+	if (nodes_weight(hv_minroot_nodes) > 0)
+		hv_minroot_nodes_defined = true;
+
+	return 0;
+}
+early_param("hv_root_vp_nodes", hv_parse_root_vp_nodes);
 
 #if defined(CONFIG_SMP) && IS_ENABLED(CONFIG_HYPERV)
 static void __init hv_smp_prepare_boot_cpu(void)
