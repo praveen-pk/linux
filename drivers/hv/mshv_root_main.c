praveen-pk/linux

commit e48d722ab0dc752dad3375655d58f60dae752903

Drivers: hv: map the VP stats page only for root partition
In the 26100 build of the hypervisor that we have in the VHD, only the
root partition is allowed to map the VP stats pages. An L1VH partition
is not allowed to map it. Check if we are in a root partition before
issuing the hypercall.

This failure causes guest creation to fail resulting in all tests
failing in the L1VH pipeline.

NOTE:
In the latest development branch of the hypervisor, L1VH partitions are
allowed to map the VP stats page. However, there is a change in ABI.
Once those builds are released, this restriction should be lifted.

Signed-off-by: Anirudh Rayabharam <anrayabh@linux.microsoft.com>

drivers/hv/mshv_root_main.c
@@ -1154,14 +1154,18 @@ mshv_partition_ioctl_create_vp(struct mshv_partition *partition,
 			goto unmap_intercept_message_page;
 	}
 
-	memset(&identity, 0, sizeof(identity));
-	identity.vp.partition_id = partition->id;
-	identity.vp.vp_index = args.vp_index;
-	identity.vp.flags = 0;
+	/* L1VH partitions are not allowed to map the stats page. Yet. */
+	if (hv_root_partition()) {
+		memset(&identity, 0, sizeof(identity));
+		identity.vp.partition_id = partition->id;
+		identity.vp.vp_index = args.vp_index;
+		identity.vp.flags = 0;
 
-	ret = hv_call_map_stat_page(HV_STATS_OBJECT_VP, &identity, &stats_page);
-	if (ret)
-		goto unmap_register_page;
+		ret = hv_call_map_stat_page(HV_STATS_OBJECT_VP, &identity,
+					&stats_page);
+		if (ret)
+			goto unmap_register_page;
+	}
 
 	vp = kzalloc(sizeof(*vp), GFP_KERNEL);
 	if (!vp)
@@ -1219,7 +1223,8 @@ free_registers:
 free_vp:
 	kfree(vp);
 unmap_stats_page:
-	hv_call_unmap_stat_page(HV_STATS_OBJECT_VP, &identity);
+	if (hv_root_partition())
+		hv_call_unmap_stat_page(HV_STATS_OBJECT_VP, &identity);
 unmap_register_page:
 	if (!mshv_partition_isolation_type_snp(partition))
 		hv_call_unmap_vp_state_page(partition->id, args.vp_index,

commit 76d9c1b7971e962c8737049ddae628c1bc8a3400

fixup! Drivers: hv: map the VP stats page only for root partition
Unmap the VP stats page only if we are in a root partition.

Fixes: e48d722ab0dc ("Drivers: hv: map the VP stats page only for root partition")
Signed-off-by: Anirudh Rayabharam <anrayabh@linux.microsoft.com>

drivers/hv/mshv_root_main.c
@@ -2601,14 +2601,17 @@ static void destroy_partition(struct mshv_partition *partition)
 
 		mshv_debugfs_vp_remove(vp);
 
-		memset(&identity, 0, sizeof(identity));
-		identity.vp.partition_id = partition->id;
-		identity.vp.vp_index = vp->index;
-		identity.vp.flags = 0;
+		if (vp->stats_page) {
+			memset(&identity, 0, sizeof(identity));
+			identity.vp.partition_id = partition->id;
+			identity.vp.vp_index = vp->index;
+			identity.vp.flags = 0;
 
-		(void)hv_call_unmap_stat_page(HV_STATS_OBJECT_VP, &identity);
+			(void)hv_call_unmap_stat_page(HV_STATS_OBJECT_VP,
+						&identity);
 
-		vp->stats_page = NULL;
+			vp->stats_page = NULL;
+		}
 
 		if (vp->register_page) {
 			(void)hv_call_unmap_vp_state_page(partition->id, vp->index,