505cfd6a3b597823ef2de5691060634cb1c78261
Praveen K Paladugu <prapal@linux.microsoft.com>
2024-05-09 13:06:27 -0500

    hv:diag: replace mshv ref time in diag messages
    Replace mshv reference time in Diagnostic messages with real ktime in secs.
    
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>

--- drivers/hv/diag/mshv_diaglog.c
+++ drivers/hv/diag/mshv_diaglog.c
@@ -26,6 +26,8 @@
 #include <asm/mshyperv.h>
 #include <uapi/hyperv/hvtrapi.h>
 
+static size_t MSHV_TICKS_PER_SEC = NSEC_PER_SEC/100; /* 1 tick is 100 ns long in mshv */
+
 /* Context saved in FDs private_data */
 struct fd_ctx {
 	struct hv_eventlog_buffer_header *cur_buf;
@@ -130,14 +132,32 @@ static bool validate_buf_and_chk_newdata(struct fd_ctx *fd_ctx)
 static int copy_next_record(struct fd_ctx *fd_ctx, char __user *ubuf,
 			    int ubuf_remain)
 {
+	s64 offset_secs;
 	struct hv_eventlog_entry_header *entry = get_next_entry_ptr(fd_ctx);
+	struct hv_eventlog_entry_header tmp_header;
+	size_t hv_eventlog_entry_header_sz =
+					sizeof(struct hv_eventlog_entry_header);
+
+
+	offset_secs = (s64)(fd_ctx->mshv_ref_time0 - entry->time_stamp)/
+			   (s64)MSHV_TICKS_PER_SEC;
+
+	memcpy(&tmp_header, entry, hv_eventlog_entry_header_sz);
+	tmp_header.time_stamp = fd_ctx->ktime_real0 - offset_secs;
 
 	if (entry == NULL || entry->type == 0)
 		return 0;
 	if (entry->size > ubuf_remain)
 		return 0;
 
-	if (copy_to_user(ubuf, entry, entry->size))
+	/* Copy the modified header first */
+	if (copy_to_user(ubuf, &tmp_header, hv_eventlog_entry_header_sz))
+		return -EFAULT;
+
+	/* Copy rest of the record */
+	if (copy_to_user((char *) ubuf + hv_eventlog_entry_header_sz,
+			 (char *) entry + hv_eventlog_entry_header_sz,
+			 entry->size - hv_eventlog_entry_header_sz))
 		return -EFAULT;
 
 	/* next entry rec is at entry size plus any padding added by hyp */
