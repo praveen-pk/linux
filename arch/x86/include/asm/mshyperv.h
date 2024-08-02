5ca611ee5706b14194b772b109edeb7dbc1c2bcc
Nuno Das Neves <nudasnev@microsoft.com>
2024-08-02 17:26:44 +0000

    mshv: Port mshv kdump code from 5.15
    NOTE: This is not complete - the code doesn't work yet due to changes in
    kexec/kdump since 5.15.
    
    Objtool produces some warnings.
    
    Added ANNOTATE_RETPOLINE_SAFE to jmp in hv_trampoline.S
    
    087fc7d2d910 mshv: add support for mshv kdump-kexec crash dump collection
    6478bb679a3f Drivers:hv: support linux only core collection
    5b74e8dd2a6b hv: enable hypervisor crash collection and add notify
    
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -41,6 +41,7 @@ static inline unsigned char hv_get_nmi_reason(void)
 #if IS_ENABLED(CONFIG_HYPERV)
 extern int hyperv_init_cpuhp;
 extern bool hyperv_paravisor_present;
+extern int hv_crash_enabled;
 
 extern void *hv_hypercall_pg;
 
