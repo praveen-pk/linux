7d947adabacce316d53ea5d9fae06476956d9d16
Jinank Jain <jinankjain@linux.microsoft.com>
2025-03-18 11:29:30 +0530

    arch/x86/hyperv: Add support for RTC for CVM guest
    Without RTC support CVM guest cannot sync time with host in case NTP is
    not enabled on the guest side. So as a fallback option let's enable
    support for RTC if guest wants to sync time with host.
    
    Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

--- arch/x86/hyperv/ivm.c
+++ arch/x86/hyperv/ivm.c
@@ -617,8 +617,6 @@ __init void hv_sev_init_mem_and_cpu(void)
 	 */
 	x86_platform.legacy.rtc			= 0;
 	x86_platform.legacy.reserve_bios_regions = 0;
-	x86_platform.set_wallclock		= set_rtc_noop;
-	x86_platform.get_wallclock		= get_rtc_noop;
 	x86_init.resources.probe_roms		= x86_init_noop;
 	x86_init.resources.reserve_resources	= x86_init_noop;
 	x86_init.mpparse.find_smp_config	= x86_init_noop;
